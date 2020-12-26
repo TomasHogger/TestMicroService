8dd383cc2e99297ba3ecf1a2eeb8ec4d42bea5ba
aavdeev01 <aavdeev01@icloud.com>
2020-12-26 22:08:57 +0300

    init customers microservice

---
+++ microservice.customers/src/test/java/com/example/microservice/customers/microservice/customers/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservice.customers.microservice.customers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
